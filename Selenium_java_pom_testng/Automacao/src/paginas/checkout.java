0b8d57e49066ec8dcbb90edfae06330037389821
Antonio <antonio.martins@yahoo.com.br>
2023-12-19 20:22:11 -0300

    subindo

---
+++ Selenium_java_pom_testng/Automacao/src/paginas/checkout.java
@@ -0,0 +1,47 @@
+package paginas;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class checkout {
+	
+	WebDriver driver;
+	
+	//Localizadores
+	By btncheckout = By.xpath("//*[@id=\"cart_contents_container\"]/div/div[2]/a[2]");
+	By field_firstname = By.xpath("//input[@id='first-name']");
+	By field_lastname = By.xpath("//input[@id='last-name']");
+	By field_zipcode = By.xpath("//input[@id='postal-code']");
+	By btncontinue = By.xpath("//input[@value='CONTINUE']");
+	
+	//Construtor que ira automaticament chamar o objeto da classe criada
+	public checkout(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	//Metodo para clicar no botão de checkout
+	public void fazerCheckout() {
+		driver.findElement(btncheckout).click();
+	}
+
+	//Metodo para informar o Primeiro Nome
+	public void inserirPrimeiroNome() {
+		driver.findElement(field_firstname).sendKeys("Maria");
+	}
+
+	//Metodo para informar o Ultimo Nome
+	public void inserirUltimoNome() {
+		driver.findElement(field_lastname).sendKeys("de Lourdes");
+	}
+
+	//Metodo para informar o Codigo Postal
+	public void inserirZipPostalCode() {
+		driver.findElement(field_zipcode).sendKeys("20240000");
+	}
+	
+	//Metodo para clicar no botão de continuar
+	public void continuar() {
+		driver.findElement(btncontinue).click();
+	}
+
+}
---
+++ Selenium_java_pom_testng/Automacao/src/paginas/selecionarproduto.java
@@ -0,0 +1,28 @@
+package paginas;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class selecionarproduto {
+	
+	WebDriver driver;
+	
+	//Localizadores
+	By addtocart = By.xpath("//div[@class='inventory_list']//div[1]//div[3]//button[1]");
+	By carrinho = By.xpath("//*[@id='shopping_cart_container']/a/span");
+	
+	//Construtor que ira automaticament chamar o objeto da classe criada
+	public selecionarproduto(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	//Metodo para adicionar um produto na cesta de carrinho
+	public void adicionarproduto() {
+		driver.findElement(addtocart).click();
+	}
+
+	//Metodo para acessar o carrinho
+	public void acessarcarrinho() {
+		driver.findElement(carrinho).click();
+	}
+}
